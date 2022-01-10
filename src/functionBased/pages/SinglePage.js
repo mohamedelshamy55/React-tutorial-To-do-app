14448b6bee062c7b0ca44041d53ce81837df71ee
Mohamed ElShamy <mohamedelshamy55@gmail.com>
2022-01-10 19:44:07 +0200

    Update SinglePage.js

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -18,14 +18,14 @@ const SinglePage = () => {
   const aboutContent = aboutData.find((item) => item.slug === slug);
   const { title, description } = aboutContent;
   return (
-      <div className="main__content">
-      <h1>
-    { title }
-    </h1>
-      <p>
-      { description }
-    </p>
-    </div>
+    <div className="main__content">
+        <h1>
+        { title }
+      </h1>
+        <p>
+        { description }
+      </p>
+      </div>
   );
 };
 export default SinglePage;
